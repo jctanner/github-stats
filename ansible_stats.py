cbd7ae3367b553fa215ba6625c05062bd093d85b
James Tanner <tanner.jc@gmail.com>
2016-05-05 23:19:12 -0400

    Aggregrate the stats across all repos

--- ansible_stats.py
+++ ansible_stats.py
@@ -4,6 +4,8 @@ import os
 from GithubStats.githubstats import GithubStats
 from pprint import pprint
 
+import pandas as pd
+
 def main():
 
     #fns = ['/home/jtanner/workspace/.triage/imsweb_ezmomi.pickle']
@@ -13,6 +15,13 @@ def main():
            ('ansible_ansible-modules-core.pickle', 'results/ansible-modules-core.csv'),
            ('ansible_ansible-modules-extras.pickle', 'results/ansible-modules-extras.csv')]
 
+    xcolumns = []
+    xreponames = []
+    xdf = pd.DataFrame()
+
+    ############################################
+    # Get stats for each individual repo
+    ############################################
     for fn in fns:
         fname = os.path.join(cachedir, fn[0])
         ghs = GithubStats()
@@ -20,6 +29,37 @@ def main():
         df = ghs.process_data()
         print df.tail(5)
         df.to_csv(fn[1])
+
+        repo_name = os.path.basename(fn[0]).replace('.pickle', '')
+        xreponames.append(repo_name)
+        for col in df.columns:
+            if not col in xcolumns:
+                xcolumns.append(col)
+            newcol = col + '__' + repo_name
+            xdf[newcol] = df[col]
+
+    ############################################
+    # Aggregate all data
+    ############################################
+
+    # add totals
+    for col in xcolumns:
+        cdf = pd.DataFrame(columns=[col])
+        for repo_name in xreponames:
+            rcol = col + '__' + repo_name
+            # create index first
+            if len(cdf[col]) == 0:
+                cdf[col] = xdf[rcol].copy()
+                #import epdb; epdb.st()
+            else:
+                cdf[col] = xdf[rcol] + cdf[col]
+
+            #import epdb; epdb.st()
+        xdf[col] = cdf[col]    
+        #import epdb; epdb.st()
+
+    xdf.to_csv('results/ansible_all_repos.csv')
+    #import epdb; epdb.st()
 
 if __name__ == "__main__":
     main()
