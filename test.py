32f0b5414d2bc10c8c80708bdbcfca28090278e1
James Tanner <tanner.jc@gmail.com>
2016-05-05 21:36:45 -0400

    add test script

--- test.py
+++ test.py
@@ -1,17 +1,25 @@
 #!/opt/anaconda2/bin/python
 
+import os
 from GithubStats.githubstats import GithubStats
 from pprint import pprint
 
 def main():
 
-    fns = ['/home/jtanner/workspace/.triage/ansible_ansible-modules-core.pickle']
     #fns = ['/home/jtanner/workspace/.triage/imsweb_ezmomi.pickle']
-    ghs = GithubStats()
-    ghs.load_pickle(fns[0])
-    ghs.process_data()
-    
-    pprint(ghs.stats)
+
+    cachedir = os.path.expanduser('~/.triage')
+    fns = [('ansible_ansible.pickle', 'ansible-ansible.csv'),
+           ('ansible_ansible-modules-core.pickle', 'ansible-modules-core.csv'),
+           ('ansible_ansible-modules-extras.pickle', 'ansible-modules-extras.csv')]
+
+    for fn in fns:
+        fname = os.path.join(cachedir, fn[0])
+        ghs = GithubStats()
+        ghs.load_pickle(fname)
+        df = ghs.process_data()
+        print df.tail(5)
+        df.to_csv(fn[1])
 
 if __name__ == "__main__":
     main()
